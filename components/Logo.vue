e03cb3277911be9e59a0a0c7d9f5a97c1453172f
Daniel Graves <dgravesa@gmail.com>
2020-02-12 20:31:08 -0600

    initializing project with some reworking of initial layout; using pug

---
+++ components/Logo.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  img.waterlogger-logo(src="/waterlogger.jpg", width="245", height="245")
+</template>
+
+<style>
+.waterlogger-logo {
+  animation: 500ms appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
