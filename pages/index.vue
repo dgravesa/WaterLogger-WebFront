58a05e980f743c8e62c5d5fde35b8cb8f1c0a9ee
Daniel Graves <dgravesa@gmail.com>
2020-02-12 23:46:51 -0600

    getting the gist of things with faux login/logout

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,26 @@
 <template lang="pug">
   .container
-    div
+    div(v-if="loggedInUser == ''")
       Logo
       h1.title WaterLogger
+
       input.login-field(v-model="userEmail", type="text", placeholder="Email")
       br
       input.login-field(v-model="userPassword", type="password", placeholder="Password")
       br
       button.button--grey(@click="attemptLogin") Login
+
+      p.error-message(v-if="loginErrorMessage != ''") {{loginErrorMessage}}
+
       p.subtext Don't have an account? 
         // TODO: update href
         a.subtext-link(href="https://github.com/nuxt/nuxt.js", target="_blank") Sign up
+
+    // TODO: best for these to get moved to their own components
+    div(v-else)
+      h1.title Hello, {{loggedInUser}}!
+      p.subtext Today's total: {{totalToday}}
+      button.button--grey(@click="logout") Logout
 </template>
 
 <script>
@@ -19,8 +29,22 @@ import Logo from '~/components/Logo.vue'
 export default {
   data() {
     return {
-      userEmail: "",
-      userPassword: "",
+      userEmail: '',
+      userPassword: '',
+      loginErrorMessage: '',
+      loggedInUser: '', // TODO: change this when Firebase auth is up and going
+      drinkLogs: [  // TODO: pull this from server
+        { time: "", amount: 7.5 },
+        { time: "", amount: 16 },
+        { time: "", amount: 12 },
+      ]
+    }
+  },
+  computed: {
+    totalToday: function () {
+      return this.drinkLogs.reduce((total, drinklog) => {
+        return total + drinklog.amount;
+      }, 0);
     }
   },
   components: {
@@ -28,8 +52,22 @@ export default {
   },
   methods: {
     attemptLogin: function (event) {
-      console.log("received login attempt:", this.userEmail, this.userPassword)
+      // TODO: make validation logic depend on an actual authentication call
+      // For now, this just says invalid when supposed email address is missing an @ symbol
+      if (this.userEmail.includes("@")) {
+        this.loggedInUser = this.userEmail;
+        this.loginErrorMessage = '';
+
+        // simulate login success for now
+        this.userEmail = '';
+        this.userPassword = '';
+      } else {
+        this.loginErrorMessage = 'Invalid email or password.';
+      }
     },
+    logout: function(event) {
+      this.loggedInUser = '';
+    }
   },
 }
 </script>
@@ -85,5 +123,13 @@ export default {
   padding: 10px 30px;
   margin: 0px 0px 10px 0px;
   width: 300px;
+}
+
+.error-message {
+  font-weight: 300;
+  font-size: 18px;
+  color: indianred;
+  word-spacing: 2px;
+  padding-top: 10px;
 }
 </style>
